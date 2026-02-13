9dd39aed92069e8c46a7f5120da6835d5e05fb3d
racha334 <rachabahlak@gmail.com>
2026-02-13 23:54:40 +0200

    Products Cleaning

---
+++ transform/02_clean_products.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df_raw = pd.read_csv("data/raw/olist_products_dataset.csv")
+df = df_raw.copy()
+
+df = df.apply(lambda col: col.str.strip() if col.dtype == "object" else col)
+df['product_category_name'] = df['product_category_name'].fillna("unknown")
+df['product_category_name'] = df['product_category_name'].str.lower().str.replace(" ", "_", regex=False)
+
+df.to_csv("data/processed/products_clean.csv", index=False)
