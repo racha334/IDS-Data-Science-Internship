1e1690efdbc1b3f0ce0d9c3a5ab31185c2739bd0
racha334 <rachabahlak@gmail.com>
2026-02-15 20:08:14 +0200

    rename product columns

--- transform/02_clean_products.py
+++ transform/02_clean_products.py
@@ -7,4 +7,6 @@ df = df.apply(lambda col: col.str.strip() if col.dtype == "object" else col)
 df['product_category_name'] = df['product_category_name'].fillna("unknown")
 df['product_category_name'] = df['product_category_name'].str.lower().str.replace(" ", "_", regex=False)
 
+df = df.rename(columns={"product_name_lenght": "product_name_length", "product_description_lenght": "product_description_length"})
+
 df.to_csv("data/processed/products_clean.csv", index=False)
